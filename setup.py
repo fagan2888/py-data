38377bcb0bb0574834004415a57aec367edda643
Trijeet <uniquetrij@gmail.com>
2019-02-07 20:36:23 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='py_data',
+    version='0.0.1',
+    description="Common data interface",
+    url='https://github.com/uniquetrij/py-data',
+    author='Trijeet Modak',
+    author_email='uniquetrij@gmail.com',
+    packages=['py_data'],
+    zip_safe=False
+)
