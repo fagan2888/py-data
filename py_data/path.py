da5ddeab8df7f2c00f09b55a28df4340e60b0037
Trijeet Modak <uniquetrij@gmail.com>
2019-04-17 19:33:09 +0530

    obtain path even if not exist

--- py_data/path.py
+++ py_data/path.py
@@ -1,10 +1,15 @@
 from os.path import dirname, realpath, exists
 
 
-def get(file_name=None):
+def generate(file_name=None):
     if not file_name:
         return dirname(realpath(__file__))
-    path = get() + "/" + file_name
+    path = generate() + "/" + file_name
+    return path
+
+
+def get(file_name=None):
+    path = generate(file_name)
     if exists(path):
         return path
     return None
